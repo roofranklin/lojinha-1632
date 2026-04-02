2d007e303b4a23f1e4af25120bdb5cb43af2b9a4
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2026-04-02 19:30:39 -0300

    exercicio aula 01 finalizado

---
+++ src/app/components/card-produto/card-produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-produto',
+  imports: [],
+  templateUrl: './card-produto.component.html',
+  styleUrl: './card-produto.component.css'
+})
+export class CardProdutoComponent {
+
+}
--- src/app/components/lista-produtos/lista-produtos.component.ts
+++ src/app/components/lista-produtos/lista-produtos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardProdutoComponent } from '../card-produto/card-produto.component';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [CardProdutoComponent],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.css'
 })
